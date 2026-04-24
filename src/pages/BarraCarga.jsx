65a653d77f30d372ae4086e8c3f49b0e8cb93904
Gardansito <daniel.alejandro.garcia@correounivalle.edu.co>
2026-04-23 22:42:23 -0500

    a
    a

--- src/pages/BarraCarga.jsx
+++ src/pages/BarraCarga.jsx
@@ -156,7 +156,7 @@ export function ModalConflicto({ conflicto, nuevasHoras, onElgirFecha, onAjustar
       if (tipo === 'fecha') onElgirFecha()
       else if (tipo === 'horas') onAjustarHoras()
       else onGuardarIgual()
-    }, 9000)
+    }, 1500)
   }
 
   return (
